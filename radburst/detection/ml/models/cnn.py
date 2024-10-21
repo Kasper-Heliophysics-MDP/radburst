35228b67a1ab749b0436a83a47b48bba89218633
Cameron Burdgick <cburdgic@umich.edu>
2024-10-20 20:52:05 -0400

    Upload old cnn model

---
+++ radburst/detection/ml/models/cnn.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=(3, 3))
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.bn2 = nn.BatchNorm2d(32)
+
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.bn3 = nn.BatchNorm2d(64)
+
+        self.conv4 = nn.Conv2d(64, 8, kernel_size=3, padding=1)
+        self.pool4 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.bn4 = nn.BatchNorm2d(8)
+
+        self.dropout1 = nn.Dropout2d(p=0.5)
+        
+        self.fc1 = nn.Linear(8*8*8, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool4(x)
+
+        x = torch.flatten(x, 1)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
