2d97d7679d45d6bf8ff5825ddadb7bffd8d3d3c1
Cameron Burdgick <cburdgic@umich.edu>
2024-10-30 15:50:42 -0400

    Update detection documentation

--- docs/code_reference/detection/detection.md
+++ docs/code_reference/detection/detection.md
@@ -1,6 +1,4 @@
-# Solar Radio Burst Detection
-
-## Overview
+# Burst Detection Overview
 
 This module focuses on the detection of solar radio bursts from spectrogram data. The detection process is a crucial first step in identifying and classifying these phenomena.
 
@@ -8,9 +6,11 @@ This module focuses on the detection of solar radio bursts from spectrogram data
 
 1. **Preprocessing**: Before detection, the spectrogram data typically undergoes preprocessing steps such as noise reduction and normalization. These steps are crucial for improving the detection accuracy.
 
-2. **Detection Algorithm**: The core of this module is the burst detection algorithm. While the specific implementation details are not provided in the current codebase, common approaches include:
-   - Drift Rate-based detection
-   - Machine learning-based anomaly detection
+2. **Detection Algorithm**: The core of this module is the burst detection algorithm. While not all methods are implemented in this codebase, refer to the following pages for more information on each approach:
+      - [Drift Rate-Based Detection](detection_drift_rate.md)
+      - [Machine Learning-Based Anomaly Detection](detection_ml.md)
+      - [Morphological Operations for Identifying Candidate Regions](detection_morph_ops.md)
+
 
 3. **Post-processing**: After initial detection, post-processing steps may be applied to refine the results, such as merging nearby detections or filtering out false positives.
 
--- radburst/utils/preprocessing.py
+++ radburst/utils/preprocessing.py
@@ -39,8 +39,8 @@ def remove_vertical_lines(arr, num_std = 5, dist = 10):
         dist (int): Distance away from vertical line column to get replacement column.
         
     Returns:
-        np.ndarray: Processed array with vertical lines removed."""
-    
+        np.ndarray: Processed array with vertical lines removed.
+    """
     # Calculate variance of each column
     vars = np.var(arr, axis=0, ddof=0)
 
@@ -65,13 +65,36 @@ def remove_vertical_lines(arr, num_std = 5, dist = 10):
 
 
 def stan_rows_remove_verts(arr):
-    """Standardize rows and remove vertical lines"""
+    """Standardize rows and remove vertical lines from spectrogram array.
+    
+    Args:
+        arr (np.ndarray): Array to process.
+        
+    Returns:
+        np.ndarray: Processed array - standardized rows and vertical lines removed.
+    """
     res = standardize_rows(arr)
     res = remove_vertical_lines(res)
     return res
 
 
 class BinaryMaskRegion:
+    """Represents a bounding box (bbox) for a connected component in a binary mask.
+    
+    Attributes:
+        min_row (int): The minimum row index of the bounding box.
+        min_col (int): The minimum column index of the bounding box.
+        max_row (int): The maximum row index of the bounding box.
+        max_col (int): The maximum column index of the bounding box.
+        height (int): The height of the bounding box.
+        width (int): The width of the bounding box.
+        hw_ratio (float): The height-to-width ratio of the bounding box.
+        area (int): The area of the bounding box.
+
+    Args:
+        bbox (tuple): A tuple containing the bounding box coordinates
+                      in the format (min_row, min_col, max_row, max_col).
+    """
     def __init__(self, bbox):
         self.min_row, self.min_col, self.max_row, self.max_col = bbox
         self.height = self.max_row - self.min_row
@@ -81,6 +104,14 @@ class BinaryMaskRegion:
 
 
 class RegionManager:
+    """Manages a collection of regions and provides methods to analyze/filter them.
+    
+    This class allows adding regions, finding the two largest regions and filtering 
+    based on specific criteria such as row difference and size ratio.
+    
+    Attributes:
+        regions (list): A list of `BinaryMaskRegion` instances that represent the regions.  
+    """
     def __init__(self):
         self.regions = []
 
@@ -88,11 +119,24 @@ class RegionManager:
         self.regions.append(region)
 
     def _get_largest_2_regions(self):
+        """Return up to two of the largest regions."""
         if len(self.regions) < 2: return self.regions
         sorted_area = sorted(self.regions, key=lambda r: r.area)
         return sorted_area[-2:]
     
     def filter_largest_2_regions(self, row_diff_threshold=50, size_ratio_threshold=10):
+        """Filters the two largest regions based on criteria.
+
+        Checks the difference in max row indices and area ratio of the largest regions
+        to determine which regions to keep.
+        
+        Args:
+            row_diff_threshold (int):
+            size_ratio_threshold (int):
+            
+        Returns:
+            list: A list containing the filtered `BinaryMaskRegion` instances.
+        """
         if len(self.regions) < 2: return self.regions
 
         # sometimes mask images have one large low (high row) region and one small high (low row) region 
@@ -117,17 +161,42 @@ class RegionManager:
   
 
 def create_binary_mask(arr, pct_threshold = 95):
+    """Create a binary mask of an array.
+    
+    Args:
+        arr (np.ndarray): Array to create mask.
+        pct_threshold (int): Percentile threshold for setting mask values to 1.
+                             For example, if this value is 95, the top 95% of array 
+                             values will be 1, the rest 0.
+        
+    Returns:
+        np.ndarray: Binary mask where values above threshold are 1 and others are 0
+    """
     threshold = np.percentile(arr, pct_threshold)
     return arr > threshold
 
 
-def morph_ops(arr, erosion_struct_size = (10,3), dilation_struct_size = (1,5)):
-    """Make array binary and perform morphological operations to remove small components."""
-    arr = binary_dilation(arr, structure=np.ones((3,20)))
+def morph_ops(binary_arr, 
+              dilation_1_struct_size = (3,20),
+              erosion_struct_size = (10,3), 
+              dilation_2_struct_size = (1,5)):
+    """Apply morphological operations to a binary mask to refine components/structures.
 
-    eroded_mask = binary_erosion(arr, structure=np.ones(erosion_struct_size))
-    dilation_mask = binary_dilation(eroded_mask, structure=np.ones(dilation_struct_size))
-    return dilation_mask
+    The goal is to remove small structures and enhance larger structures (potential bursts).
+    
+    Args:
+        binary_arr (np.ndarray): Input binary array (mask) to be processed.
+        dilation_1_struct_size (tuple): Structure element size for first dilation step.
+        erosion_struct_size (tuple): Structure element size for erosion step.
+        dilation_2_struct_size (tuple): Structure element size for second dilation step.
+        
+    Returns:
+        np.ndarray: Refined binary mask with morphological operations applied.
+    """
+    dilation_1_mask = binary_dilation(binary_arr, structure=np.ones(dilation_1_struct_size))
+    eroded_mask = binary_erosion(dilation_1_mask, structure=np.ones(erosion_struct_size))
+    dilation_2_mask = binary_dilation(eroded_mask, structure=np.ones(dilation_2_struct_size))
+    return dilation_2_mask
 
 
 def filtered_components(mask, 
@@ -135,7 +204,29 @@ def filtered_components(mask,
                         min_area = 600, 
                         min_h_wide_tall = 30, 
                         min_area_wide_tall = 1000):
-    """Only keep connected components (regions) in binary mask that meet criteria."""
+    """Filter connected components (regions) in a binary mask based on size and shape criteria.
+
+    First, filter out regions by two sets of criteria (main and secondary) that evaluate
+    height-to-width ratio and area. Then, keep upto the two largest remaining regions and 
+    apply two additional criteria from the RegionManager class: area ratio and max row difference to
+    reduce false positive detections of a second burst.
+
+    The secondary criteria (`keep_wide_tall_bursts`) was added to include type 2 bursts, which
+    the main criteria exclude due to the restrictive height-to-width ratio.
+
+    Args:
+        mask (np.ndarray): Binary mask of connected components to filter.
+        min_hw_ratio (float): Minimum height-to-width ratio for main filter criteria.
+        min_area (int): Minimum area for main filter criteria.
+        min_h_wide_tall (int): Minimum height-to-widt ratio for keep_wide_tall_bursts filter criteria.
+        min_area_wide_tall (int): Minimum area for keep_wide_tall_bursts filter criteria.
+
+    Returns:
+        tuple: A tuple containing:
+            - filtered_largest_largest_2_regions (list): List of BinaryMaskRegion objects - regions that passed 
+                                                         additional criteria found in RegionManager class. 
+            - filtered_mask (np.ndarray): Mask with only regions that passed main and secondary criteria.
+    """
     
     filtered_mask = np.zeros_like(mask)
     region_manager = RegionManager()
@@ -166,4 +257,13 @@ def filtered_components(mask,
 
 
 def blur(arr, blur_filter_shape = (51,11)):
+    """Blur array using a Gaussian kernel to enhance potential bursts.
+
+    Args:
+        arr (np.ndarray): Input array to blur.
+        blur_filter_shape (tuple): Gaussian kernel size (width, height).
+
+    Returns:
+        np.ndarray: Blurred array.
+    """
     return cv2.GaussianBlur(arr,blur_filter_shape,0)  
